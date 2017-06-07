aacba808bed21f648247b97767f821686acb8d6f
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-07 15:39:31 -0500

    added helpers to app/edited signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-
 	configure do
 		set :public_folder, 'public'
 		set :views, 'app/views'
 		enable :sessions
 		set :session_secret, "secret"
 	end
-
 
 	get '/' do
 		erb :index
@@ -19,4 +17,24 @@ class ApplicationController < Sinatra::Base
 		erb :'/users/signup'
 	end
 	
+	post '/signup' do
+		@user = User.create(username: params[:username], email: params[:email], password: params[password])
+		@session = session
+		@session[:user_id] = @user.id
+		if logged_in? && !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
+			redirect '/index'
+		else
+			redirect '/signup'
+		end
+	end
+
+	helpers do
+	    def logged_in?
+	      !!session[:user_id]
+	    end
+
+	    def current_user
+	      User.find(session[:user_id])
+	    end
+  	end
 end
