b7c00cb2641069ad75dfa4b66818760ba6852638
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-06 22:40:36 -0500

    made files for models

--- db/migrate/20170607032811_create_parts.rb
+++ db/migrate/20170607032811_create_parts.rb
@@ -1,4 +1,9 @@
 class CreateParts < ActiveRecord::Migration[5.1]
   def change
+  	create_table :parts do |t|
+		t.string :name
+		t.integer :quantity
+		t.integer :user_id
+  	end
   end
 end
--- db/migrate/20170607032829_create_manufacturers.rb
+++ db/migrate/20170607032829_create_manufacturers.rb
@@ -1,4 +1,9 @@
 class CreateManufacturers < ActiveRecord::Migration[5.1]
   def change
+  	create_table :manufacturers do |t|
+		t.string :name
+		t.integer :user_id
+		t.integer :part_id
+  	end
   end
 end
