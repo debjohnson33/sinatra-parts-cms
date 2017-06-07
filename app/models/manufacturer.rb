22a6f64b60fddc325adc29baba64e461f951677a
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-07 10:59:32 -0500

    added associations to models

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,3 +1,4 @@
 class Manufacturer < ActiveRecord::Base
-	
+	belongs_to :user
+	belongs_to :parts
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ActiveRecord::Base
-	
+	belongs_to :user
+	belongs_to :manufacturer
 end
