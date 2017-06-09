54f61ca392e27df7018b25a47876341ff12c96a9
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-09 10:39:39 -0500

    added manufacturer table/model & changed parts table

---
+++ app/models/manufacturer.rb
@@ -0,0 +1,4 @@
+class Manufacturer < ActiveRecord::Base
+	has_many :parts
+	has_many :users, through: :parts
+end
---
+++ db/migrate/20170609152855_create_manufacturers_new_table.rb
@@ -0,0 +1,7 @@
+class CreateManufacturersNewTable < ActiveRecord::Migration[5.1]
+  def change
+  	  	create_table :manufacturers do |t|
+		t.string :name
+  	end
+  end
+end
