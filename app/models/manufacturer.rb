e5c7dc2d3be444903635930614d2189a72dafdb0
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-09 20:46:13 -0500

    edited associations

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,5 +1,5 @@
 class Manufacturer < ActiveRecord::Base
 	has_many :parts
 	has_many :user_manufacturers
-	has_many :user, through: :user_manufacturers
+	has_many :users, through: :user_manufacturers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :parts
+	has_many :user_manufacturers
+	has_many :manufacturers, through: :user_manufacturers
+
 end
