bcdd61d9c9d0e0ec8c2dc0daa9f5e8561125b8f5
debjohnson33 <debjohnson_11@yahoo.com>
2017-06-27 09:24:47 -0500

    changed current user method to use find_by

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ use Rack::Flash
 	    end
 
 	    def current_user
-	      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	      @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
 	    end
   	end
 end
